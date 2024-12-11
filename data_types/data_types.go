6bbfa8ebbebb417ce7d7e7acf04d6a3aa7809003
Kirill <khanov.krl@gmail.com>
2024-12-11 22:30:26 +0100

    data types

---
+++ data_types/data_types.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func notMAin() {
+	fmt.Printf("\nType: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
+
+func main() {
+	var a bool = true    // Boolean
+	var b int = 5        // Integer
+	var c float32 = 3.14 // Floating point number
+	var d string = "Hi!" // String
+
+	fmt.Println("Boolean: ", a)
+	fmt.Println("Integer: ", b)
+	fmt.Println("Float:   ", c)
+	fmt.Println("String:  ", d)
+
+	notMAin()
+}
