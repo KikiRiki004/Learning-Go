7e481c78c1ed294f98fd45102d7fc6ddffefd6b3
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-01-04 11:32:08 -0400

    Functions Exercise

--- functions/functions.go
+++ functions/functions.go
@@ -96,3 +96,40 @@ func main() {
 
 	fmt.Println(fib(7))
 }
+
+// Example
+
+func counter() func() int {
+	count := 0 // Treasure chest
+	return func() int {
+		count++      // Update the chest
+		return count // Tell you the current value
+	}
+}
+
+// func main() {
+//     c := counter()   // Get a key to the chest
+//     fmt.Println(c()) // Output: 1
+//     fmt.Println(c()) // Output: 2
+//     fmt.Println(c()) // Output: 3
+// }
+
+// Exercise
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		result := a
+		a, b = b, a+b
+		return result
+	}
+}
+
+// func main() {
+// 	f := fibonacci()
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(f())
+// 	}
+// }
